1eb578819fb3c5fcb9e0ced3be9f2178b96a7649
Artūrs Jānis Pētersons <arturs.j.petersons@loadero.com>
2024-12-14 11:18:08 +0200

    Remove RW mutex

--- stmtcacher.go
+++ stmtcacher.go
@@ -37,7 +37,7 @@ type savedStmt struct {
 type stmtCacher struct {
 	prep  Preparer
 	cache map[string]*savedStmt
-	mu    sync.RWMutex
+	mu    sync.Mutex
 }
 
 // NewStmtCacher returns a DBProxy wrapping prep that caches Prepared Stmts.
@@ -67,15 +67,6 @@ func (sc *stmtCacher) startCleanup(maxAge time.Duration) {
 }
 
 func (sc *stmtCacher) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
-	sc.mu.RLock()
-	if s, ok := sc.cache[query]; ok {
-		s.lastUse = time.Now()
-		sc.mu.RUnlock()
-
-		return s.stmt, nil
-	}
-	sc.mu.RUnlock()
-
 	sc.mu.Lock()
 	defer sc.mu.Unlock()
 
